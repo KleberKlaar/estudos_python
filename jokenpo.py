eae1817199aa45ec2f7e638bae6e03391193a899
KleberKlaar <kklaar1988@gmail.com>
2022-10-28 19:55:12 -0300

    Jogo simples de JOKENPO.

---
+++ jokenpo.py
@@ -0,0 +1,75 @@
+#Inicializando o jogo
+from os import system
+from random import randint
+jogo = 1
+while jogo == 1:
+    print('JOKENPÔ\n\nCriado por: Kleber Klaar\n')
+    print('Escolha sua jogada:\n')
+    escolha = int(input('[1] - Pedra\n[2] - Papel\n[3] - Tesoura\nEscolha:'))
+    cpu = randint(1,3)
+    if escolha == 1:
+        texto = "PEDRA"
+    elif escolha == 2:
+        texto = "PAPEL"
+    elif escolha == 3:
+        texto = "TESOURA"
+    else:
+        print('Opção inexistente.')
+        jogo = 0
+    if cpu == 1:
+        texto_cpu = "PEDRA"
+    elif cpu == 2:
+        texto_cpu = "PAPEL"
+    elif cpu == 3:
+        texto_cpu = "TESOURA"
+    
+    if escolha == cpu:
+        print('Jogador usou {} x CPU usou {}. Empate!'.format(texto, texto_cpu))
+        novo = str(input('Jogar novamente? [S/N] '))
+        if novo == 'S' or novo == 's':
+            system('cls')
+        else:
+            jogo = 0
+    else:
+        if escolha == 1 and cpu == 3:
+            print('Jogador usou {} x CPU usou {}. Jogador venceu!'.format(texto, texto_cpu))
+            novo = str(input('Jogar novamente? [S/N] '))
+            if novo == 'S' or novo == 's':
+                system('cls')
+            else:
+                jogo = 0
+        elif escolha == 2 and cpu == 1:
+            print('Jogador usou {} x CPU usou {}. Jogador venceu!'.format(texto, texto_cpu))
+            novo = str(input('Jogar novamente? [S/N] '))
+            if novo == 'S' or novo == 's':
+                system('cls')
+            else:
+                jogo = 0
+        elif escolha == 3 and cpu == 2:
+            print('Jogador usou {} x CPU usou {}. Jogador venceu!'.format(texto, texto_cpu))
+            novo = str(input('Jogar novamente? [S/N] '))
+            if novo == 'S' or novo == 's':
+                system('cls')
+            else:
+                jogo = 0
+        elif escolha == 1 and cpu == 2:
+            print('Jogador usou {} x CPU usou {}. CPU venceu!'.format(texto, texto_cpu))
+            novo = str(input('Jogar novamente? [S/N] '))
+            if novo == 'S' or novo == 's':
+                system('cls')
+            else:
+                jogo = 0
+        elif escolha == 2 and cpu == 3:
+            print('Jogador usou {} x CPU usou {}. CPU venceu!'.format(texto, texto_cpu))
+            novo = str(input('Jogar novamente? [S/N] '))
+            if novo == 'S' or novo == 's':
+                system('cls')
+            else:
+                jogo = 0
+        elif escolha == 3 and cpu == 1:
+            print('Jogador usou {} x CPU usou {}. CPU venceu!'.format(texto, texto_cpu))
+            novo = str(input('Jogar novamente? [S/N] '))
+            if novo == 'S' or novo == 's':
+                system('cls')
+            else:
+                jogo = 0
