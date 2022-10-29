2f7beb680ae91ac78262dd6b623af4d517bc3afb
KleberKlaar <kklaar1988@gmail.com>
2022-10-29 11:24:55 -0300

    Progressão Aritmética

--- progressa_aritmetica.py
+++ progressa_aritmetica.py
@@ -1,8 +1,18 @@
 v = int(input('Digite o primeiro valor da Progressão Aritmética: '))
 r = int(input('Digite a razão da Progressão Aritmética: '))
 c = 1
+mais = 'S'
 print(v)
 while c < 10:
     c += 1
     v += r
     print(v)
+mais = str(input('Você quer ver mais termos? [S/N] ')).upper()
+while mais == 'S':
+    t = int(input('Quantos outros termos deseja ver? '))
+    c = 0
+    while c < t:
+        c += 1
+        v += r
+        print(v)
+    mais = str(input('Você quer ver mais termos? [S/N] ')).upper()
